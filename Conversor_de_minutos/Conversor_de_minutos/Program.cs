2ad2351552897c73e3ea2b49bce76cae55db9776
pablos5z <74545818+pablos5z@users.noreply.github.com>
2021-03-08 00:43:18 -0300

    Add files via upload

---
+++ Conversor_de_minutos/Conversor_de_minutos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Conversor_de_minutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ciclos=1, total, min=0, seg=0;
+
+            Console.Write("Escolha o tempo em minutos: ");
+            total = Convert.ToInt32(Console.ReadLine());
+
+            min = total;
+
+            string fmt = "00"; //to string servido pra dar o alinhamento usando 2 caracteres "00"
+
+            total = total * 60;
+            Console.Clear();
+
+            while (ciclos <= total)
+            {
+                Console.WriteLine("{0}:{1}\n", min.ToString(fmt), seg.ToString(fmt));
+                seg--;
+
+                if(seg <= 0)
+                {
+                    seg = 60;
+                    min--;
+                }
+
+                Thread.Sleep(1000); //da o delay a cada mensagem (esta em milisegundos)
+                Console.Clear(); //limpa 
+            }
+        }
+    }
+}
---
+++ exercicio_geo/exercicio_geo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, L, C;
+
+            Console.Write("Escolha o valor de n: "); //coluninhas
+            N = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n\nExemplo 1\n");
+            for(C=0; C<N; C++)
+            {
+                Console.Write("X ");
+
+            }
+            Console.Write("\n\nExemplo 2\n");
+            for (L = 0; L < N; L++)
+            {
+               
+            for (C = 0; C < N; C++)
+                {
+                      if(L>= C) 
+                    Console.Write("X ");
+                      else
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+
+            
+            //  \t tabulação, alinha em tabela
+        }
+    }
+}
